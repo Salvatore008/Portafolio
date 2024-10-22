e4481edce11a37136269f9961f351aaee85d5de6
Salvatore008 <94807210+Salvatore008@users.noreply.github.com>
2024-10-22 12:07:50 -0400

    Update fibonacci.asm

--- Problemas_EnsambladorMars/fibonacci.asm
+++ Problemas_EnsambladorMars/fibonacci.asm
@@ -1,16 +1,15 @@
 ###################################################################
-#			Estudiantes:Luigi Quero c.i. 30.009.785			      #
-#                       Sebastian Piñango c.i. 29.947.488         #
+#		Autor:Luigi Quero c.i. 30.009.785	          #
 ###################################################################
 
 #################################################################################
 # Las diferencias fundamentales entre este ejemplo y el presentado en           #
-# el libro radican en que aquí se emplean encabezados que anticipan             #
-# los datos a utilizar y sus respectivas direcciones. Además, en este ejemplo   #
-# se utilizan procedimientos del sistema que permiten la lectura, impresión en  #
-# pantalla y finalización de la ejecución del programa principal. Para que el   #
+# el libro radican en que aquÃ­ se emplean encabezados que anticipan             #
+# los datos a utilizar y sus respectivas direcciones. AdemÃ¡s, en este ejemplo   #
+# se utilizan procedimientos del sistema que permiten la lectura, impresiÃ³n en  #
+# pantalla y finalizaciÃ³n de la ejecuciÃ³n del programa principal. Para que el   #
 # ejemplo del libro sea un programa funcional,                                  #
-# es necesario incluir estas características.                                   #
+# es necesario incluir estas caracterÃ­sticas.                                   #
 # El ejemplo del libro se considera un "Ejemplo Academico" pues al ser un       #
 # ejemplo en papel este no necesita hacer estos pasos de inicializacion de      #
 # los registros y sus direcciones ni enviar salida por pantalla ni entrada      #
@@ -19,20 +18,20 @@
 # Programa que obtiene varios numeros Fibonacci
 .data
 fibs:.word   0 : 19         # "Vector" de palabras de memoria (32 bits = 4 bytes) que contendra los numeros fib
-size: .word  19             # Tamaño del "arreglo" del tamaño del vector
+size: .word  19             # TamaÃ±o del "arreglo" del tamaÃ±o del vector
 prompt: .asciiz "Cuanto numeros Fibonacci quieres generar? (2 <= x <= 19): " #prompt es el mensaje a mostrar en pantalla
 .text
       la   $s0, fibs        # Cargar la direccion del arreglo
-      la   $s5, size        # Cargar la dirección del tamaño del arreglo
-      lw   $s5, 0($s5)      # Cargar el tamaño del arreglo
+      la   $s5, size        # Cargar la direcciÃ³n del tamaÃ±o del arreglo
+      lw   $s5, 0($s5)      # Cargar el tamaÃ±o del arreglo
 
-# Está parte en el codigo la marcan como opcional, pero la habilite, sirve para leer la cantidad de numeros fibonacci a dar
+# EstÃ¡ parte en el codigo la marcan como opcional, pero la habilite, sirve para leer la cantidad de numeros fibonacci a dar
 pr:   la   $a0, prompt      # Lee la direccion del mensaje para una llamada a una rutina(servicio) del sistema o syscall
-      li   $v0, 4           # Especifica que syscall usará un servicio de escritura de cadenas de caracteres por pantalla
-      syscall               # Esta sección imprime la cadena
-      li   $v0, 5           # Especifica que syscall usará un servicio de lectura de enteros
+      li   $v0, 4           # Especifica que syscall usarÃ¡ un servicio de escritura de cadenas de caracteres por pantalla
+      syscall               # Esta secciÃ³n imprime la cadena
+      li   $v0, 5           # Especifica que syscall usarÃ¡ un servicio de lectura de enteros
       syscall               # Lee un numero por pantalla y lo guarda en el registro de resultados $v0.
-      bgt  $v0, $s5, pr     # Si el numero ingresado es mayor que el tamaño del "vector" salta a "pr" para volver a leer un numero
+      bgt  $v0, $s5, pr     # Si el numero ingresado es mayor que el tamaÃ±o del "vector" salta a "pr" para volver a leer un numero
       blt  $v0, $zero, pr   # Si el numero ingresado es menor a 0 salta a "pr" para volver a leer un numero
       add  $s5, $v0, $zero  # Aqui se transfiere el numero ingresado al registro con el que se va a trabajar $s5
       
@@ -45,18 +44,18 @@ pr:   la   $a0, prompt      # Lee la direccion del mensaje para una llamada a un
 loop: lw   $s3, 0($s0)      # Obtiene del "vector" F[n-2]
       lw   $s4, 4($s0)      # Obtiene del "vector" F[n-1]
       add  $s2, $s3, $s4    # F[n] = F[n-1] + F[n-2]
-      sw   $s2, 8($s0)      # Guarda el resultado de un numero Fibonacci en una nueva posición del "vector"
-      addi $s0, $s0, 4      # Aumenta en 1 numero entero la dirección del "vector"
+      sw   $s2, 8($s0)      # Guarda el resultado de un numero Fibonacci en una nueva posiciÃ³n del "vector"
+      addi $s0, $s0, 4      # Aumenta en 1 numero entero la direcciÃ³n del "vector"
       addi $s1, $s1, -1     # Desincrementa el iterador en 1
       bgtz $s1, loop        # Mientras el iterador sea mayor a 0 repetir el ciclo
       
-      # Los numeros Fibonacci ya están obtenidos u guardados en el "vector".
-      la   $a0, fibs        # Primera argumento del procedimiento "print" (dirección del "vector")
+      # Los numeros Fibonacci ya estÃ¡n obtenidos u guardados en el "vector".
+      la   $a0, fibs        # Primera argumento del procedimiento "print" (direcciÃ³n del "vector")
       add  $a1, $zero, $s5  # Segundo argumento del procedimiento "print" (Cantidad de numeros a imprimir)
       jal  print            # Lamma al procedimiento "print" para mostrar los numeros obtenido
 
-      # Está parte es equivalente a return 0 de la función "main" en c, en otras palabras finaliza el programa.
-      li   $v0, 10          # Especifica que syscall usará un servicio de salida
+      # EstÃ¡ parte es equivalente a return 0 de la funciÃ³n "main" en c, en otras palabras finaliza el programa.
+      li   $v0, 10          # Especifica que syscall usarÃ¡ un servicio de salida
       syscall               # Programa finalizado
 		
 ###############################################################
@@ -68,18 +67,18 @@ head: .asciiz  "Los numeros Fibonacci son:\n"
 print:add  $t0, $zero, $a0  # Carga en el registro temporal $t0 la direccion del "vector" para imprimir los numeros
       add  $t1, $zero, $a1  # Carga la cantidad de numeros a imprimir en $t1 (iterador)
       la   $a0, head        # Carga la direccion de la cadena a imprimir con syscall
-      li   $v0, 4           # Especifica que syscall usará un servicio de escritura de cadenas de caracteres por pantalla
+      li   $v0, 4           # Especifica que syscall usarÃ¡ un servicio de escritura de cadenas de caracteres por pantalla
       syscall               # Imprime la cadena "head"
       
 out:  lw   $a0, 0($t0)      # Carga el nuemro Fibonacci que actualmente se debe imprimir
-      li   $v0, 1           # Especifica que syscall usará un servicio de escritura de un entero por pantalla
+      li   $v0, 1           # Especifica que syscall usarÃ¡ un servicio de escritura de un entero por pantalla
       syscall               # Imprime el numero Fibonacci actual
       
       la   $a0, space       # Carga la direccion de la cadena a imprimir con syscall
-      li   $v0, 4           # Especifica que syscall usará un servicio de escritura de cadenas de caracteres por pantalla
+      li   $v0, 4           # Especifica que syscall usarÃ¡ un servicio de escritura de cadenas de caracteres por pantalla
       syscall               # Coloca un espacio en pantalla
       
-      addi $t0, $t0, 4      # Incrementa en 1 entero la dirección del "vector"
+      addi $t0, $t0, 4      # Incrementa en 1 entero la direcciÃ³n del "vector"
       addi $t1, $t1, -1     # desincrementa en 1 el iterador
       bgtz $t1, out         # Si no se han imprimido todos los numeros seguir imprimiendo
       
