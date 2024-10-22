a6cdce51c145208cc0d2a11bcae38d84f0c83610
Salvatore008 <94807210+Salvatore008@users.noreply.github.com>
2024-10-22 12:06:58 -0400

    Update column-major.asm

--- Problemas_EnsambladorMars/column-major.asm
+++ Problemas_EnsambladorMars/column-major.asm
@@ -1,16 +1,15 @@
 ###################################################################
-#			Estudiantes:Luigi Quero c.i. 30.009.785			      #
-#                       Sebastian PiÒango c.i. 29.947.488         #
+#		Autor:Luigi Quero c.i: 30.009.785         #
 ###################################################################
 
 #################################################################################
 # Las diferencias fundamentales entre este ejemplo y el presentado en           #
-# el libro radican en que aquÌ se emplean encabezados que anticipan             #
-# los datos a utilizar y sus respectivas direcciones. Adem·s, en este ejemplo   #
-# se utilizan procedimientos del sistema que permiten la lectura, impresiÛn en  #
-# pantalla y finalizaciÛn de la ejecuciÛn del programa principal. Para que el   #
+# el libro radican en que aqu√≠ se emplean encabezados que anticipan             #
+# los datos a utilizar y sus respectivas direcciones. Adem√°s, en este ejemplo   #
+# se utilizan procedimientos del sistema que permiten la lectura, impresi√≥n en  #
+# pantalla y finalizaci√≥n de la ejecuci√≥n del programa principal. Para que el   #
 # ejemplo del libro sea un programa funcional,                                  #
-# es necesario incluir estas caracterÌsticas.                                   #
+# es necesario incluir estas caracter√≠sticas.                                   #
 # El ejemplo del libro se considera un "Ejemplo Academico" pues al ser un       #
 # ejemplo en papel este no necesita hacer estos pasos de inicializacion de      #
 # los registros y sus direcciones ni enviar salida por pantalla ni entrada      #
@@ -22,12 +21,12 @@
 #  Pete Sanderson
 #  31 de Marzo de 2007
 #
-#  Para observar f·cilmente el orden orientado a las columnas, ejecute la
-#  herramienta VisualizaciÛn de referencia de memoria con su configuraciÛn
+#  Para observar f√°cilmente el orden orientado a las columnas, ejecute la
+#  herramienta Visualizaci√≥n de referencia de memoria con su configuraci√≥n
 #  predeterminada en este programa.
-#  Puede, al mismo tiempo o por separado, ejecutar el Simulador de cachÈ
+#  Puede, al mismo tiempo o por separado, ejecutar el Simulador de cach√©
 #  de datos sobre este programa para observar el rendimiento del almacenamiento
-#  en cachÈ. Compare los resultados con los del algoritmo transversal de
+#  en cach√©. Compare los resultados con los del algoritmo transversal de
 #  orden de fila principal.
 #
 #  El equivalente C/C++/Java de este programa MIPS es:
@@ -41,9 +40,9 @@
 #        }
 #     }
 #
-#  Note: El programa est· hecho para matriz de 16 x 16.  Si desea cambiar esto, es
+#  Note: El programa est√° hecho para matriz de 16 x 16.  Si desea cambiar esto, es
 #        necesario cambiar tres declaraciones.
-#        1.  La declaraciÛn del tamaÒo de almacenamiento de la matriz en "datos:"
+#        1.  La declaraci√≥n del tama√±o de almacenamiento de la matriz en "datos:"
 #	    debe cambiarse de 256 (que es 16 * 16) a #columnas * #filas.
 #        2. El "li" para inicializar $t0 debe cambiarse al nuevo numero de filas.
 #        3. El "li" para inicializar $t1 debe cambiarse al nuevo numero de columnas.
@@ -56,22 +55,22 @@ data:    .word     0 : 256       # Almacenamiento para una matriz de 16 * 16
          move     $s0, $zero     # $s0 = iterador de filas
          move     $s1, $zero     # $s1 = iterador de columnas
          move     $t2, $zero     # $t2 = el valor a ser almacenado
-#  Cada iteraciÛn del bucle almacenar· el valor $t1 incrementado en el siguiente elemento de la matriz.
-#  La direcciÛn correcta se calcula en cada iteraciÛn. desplazamiento = 4 * (iterador de filas * numero de columnas + iterador de columnas)
-#  Nota: no se intenta optimizar el rendimiento del tiempo de ejecuciÛn!
+#  Cada iteraci√≥n del bucle almacenar√° el valor $t1 incrementado en el siguiente elemento de la matriz.
+#  La direcci√≥n correcta se calcula en cada iteraci√≥n. desplazamiento = 4 * (iterador de filas * numero de columnas + iterador de columnas)
+#  Nota: no se intenta optimizar el rendimiento del tiempo de ejecuci√≥n!
 loop:    mult     $s0, $t1       # $s2 = iterador de filas * numero de columnas  (dos instrucciones en secuencia)
          mflo     $s2            # mover el al registro $s2 el valor del registro especial "lo"
          add      $s2, $s2, $s1  # $s2 += iterador de columnas
-         sll      $s2, $s2, 2    # $s2 *= 4 (desplazamiento a la izquierda de 2 bits) para tener la direccÌon con la restricciÛn de alineaciÛn
+         sll      $s2, $s2, 2    # $s2 *= 4 (desplazamiento a la izquierda de 2 bits) para tener la direcc√≠on con la restricci√≥n de alineaci√≥n
          sw       $t2, data($s2) # Guardar el valor de $t2 en la matriz
          addi     $t2, $t2, 1    # Incrementar en 1 el valor de $t2
-#  Control de bucle: si incrementamos m·s all· de la parte inferior de las columnas, reiniciamos el iterador de fila e incrementamos el de columna
-#                Si incrementamos m·s all· de la ˙ltima columna, hemos terminado.
+#  Control de bucle: si incrementamos m√°s all√° de la parte inferior de las columnas, reiniciamos el iterador de fila e incrementamos el de columna
+#                Si incrementamos m√°s all√° de la √∫ltima columna, hemos terminado.
          addi     $s0, $s0, 1    # Incrementar el iterador de columnas
          bne      $s0, $t0, loop # No se ha llegado a la parte inferior de las columnas, el ciclo sigue
          move     $s0, $zero     # Reiniciar iterador de filas
          addi     $s1, $s1, 1    # Incrementa el iterador de columnas
-         bne      $s1, $t1, loop # retroceder si no se llega al final de la matriz (m·s all· de la ˙ltima columna)
+         bne      $s1, $t1, loop # retroceder si no se llega al final de la matriz (m√°s all√° de la √∫ltima columna)
 # Hemos terminado de atravesar la matriz.
-         li       $v0, 10        # Especifica que syscall usar· un servicio de salida
+         li       $v0, 10        # Especifica que syscall usar√° un servicio de salida
          syscall                 # Programa finalizado
